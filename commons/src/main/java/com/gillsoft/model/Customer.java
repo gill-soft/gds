b5f536d1ebcb074454fa19b32c59b3825b3086d7
kashpur <a.kashpur@busfor.com>
2018-06-21 15:50:27 +0300

    Доработка структур

--- commons/src/main/java/com/gillsoft/model/Customer.java
+++ commons/src/main/java/com/gillsoft/model/Customer.java
@@ -1,8 +1,16 @@
 package com.gillsoft.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class Customer {
 	
 	private String id;
+
+	public Customer(String id) {
+		this.id = id;
+	}
 
 	public String getId() {
 		return id;
